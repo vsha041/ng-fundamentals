44a5745ec6766daec443fc267104cf7953342a34
vsha041 <varun.varunsharma@gmail.com>
2022-08-09 18:32:40 +1200

    Exploring Angular's CSS Encapsulation

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,6 +6,9 @@ import {Component} from '@angular/core';
     <div>
         <h1>Upcoming Angular Events</h1>
         <hr />
+        <div class="well">
+            <div>Hello World</div>
+        </div>
         <event-thumbnail #thumbnail [event]="eventInstance" ></event-thumbnail>
     </div>
   `
