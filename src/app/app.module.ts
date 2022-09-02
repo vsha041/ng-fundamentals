119501bf23c8a0f5eacc9f8d8c625630a85cc409
vsha041 <varun.varunsharma@gmail.com>
2022-09-03 08:25:51 +1200

    Navigating from code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import {ToastrService} from './common/toastr.service';
 import {EventDetailsComponent} from './events/event-details/event-details.component';
 import {RouterModule} from '@angular/router';
 import { appRoutes} from './routes';
+import {CreateEventComponent} from './events/create-event.component';
+
 
 @NgModule({
   imports: [
@@ -20,7 +22,8 @@ import { appRoutes} from './routes';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent
   ],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
