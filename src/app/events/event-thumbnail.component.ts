3e33192c5253ca73f0bc328bd76b3f9801653bac
vsha041 <varun.varunsharma@gmail.com>
2022-09-19 08:33:34 +1200

    Using Models for Type Safety

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IEvent} from './shared/event.model';
 
 @Component({
   selector: 'event-thumbnail',
@@ -38,7 +39,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 
 export class EventThumbnailComponent{
-  @Input() event: any;
+  @Input() event: IEvent;
 
   getStartTimeClass(): any{
     /*if (this.event && this.event.time === '8:00 am') {
